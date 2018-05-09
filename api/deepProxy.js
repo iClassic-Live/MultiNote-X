6103f20141614c3be4431a4d7bc30bd2131d7de3
iClassic-Live <1227889511@qq.com>
2018-05-09 12:58:01 +0800

    MultiNote X1.7.3
    MultiNote X1.7.3 试验版1.7.3

--- api/deepProxy.js
+++ api/deepProxy.js
@@ -3,8 +3,9 @@
     1. 使用该方法进行渲染会导致应用运行流畅度比主动调用setData进行渲染时稍有下降，
        若对应用运行流畅度有要求请不要使需要渲染的数据嵌套太深;
     2. 请尽量不要在引入本方法后再主动调用setData函数，以导致不必要的重复渲染;
-    3. 请尽量不要使用会导致原数组改变的数组方法改动data;
-    4. 请不要对data中的数组使用delete操作符删除元素; */
+    3. 由于代理渲染方法仍不完善：
+        1) 请尽量不要使用会导致原数组改变的数组方法修改data数据;
+        2) 请不要对data中的数组数据使用delete运算符删除元素 */
 module.exports = {  //需要使用该方法时在相应页面的onLoad周期require然后赋值给this.data即可
   rendering(that) {  //that参数为调用该函数所在地的this
     //返回深度代理函数的立即执行结果
@@ -30,55 +31,54 @@ module.exports = {  //需要使用该方法时在相应页面的onLoad周期requ
             }
           }
         }
-        if (!item._proxy_) {
-          item._proxy_  = true;
-          return new Proxy(item, {  //返回为item订制的代理
-            set(target, key, value, receiver) {  //当item的属性被读写时的操作
-              let condition = item instanceof Array ? key === "length" ? false : true : true;
-              if (status === "unset" && condition) {
-                status = "setting";
-                that.setData({ [(() => {  //渲染对应数据
-                  if (path !== undefined) {
-                    if (item instanceof Array) {
-                      return path + "[" + key + "]";
-                    } else return path + "." + key;
-                  } else return key;
-                })()]: value });
-                //对新写入的数据进行深度代理
-                value = deepProxy(value, (() => {
-                  if (path !== undefined) {
-                    if (item instanceof Array) {
-                      return path + "[" + key + "]";
-                    } else return path + "." + key;
-                  } else return key;
-                })(), "unset");
-              } else if (status === "setting") status = "unset";
-              return Reflect.set(target, key, value, receiver);
-            },
-            deleteProperty(target, key) {
-              status = "deleting";
-              if (item instanceof Array) {
-                item = item.slice(0, item.length - 1);
+        return new Proxy(item, {  //返回为item订制的代理
+          set(target, key, value, receiver) {  //当item的属性被读写时的操作
+            let condition = item instanceof Array ? key === "length" ? false : true : true;
+            if (status === "unset" && condition) {
+              status = "setting"; //设定渲染状态为写入中
+              that.setData({
+              [(() => {  //渲染对应数据
                 if (path !== undefined) {
-                  that.setData({ [path]: item });
-                } else that.setData(item);
-                status = "unset";
-              } else {
-                (function loop() {
-                  setTimeout(() => {
-                    if (!(key in item)) {
-                      if (path !== undefined) {
-                        that.setData({ [path]: item });
-                      } else that.setData({ [key]: null });
-                      status = "unset";
-                    } else loop();
-                  })
-                })()
-              }
-              return Reflect.deleteProperty(target, key);
+                  if (item instanceof Array) {
+                    return path + "[" + key + "]";
+                  } else return path + "." + key;
+                } else return key;
+              })()]: value
+              });
+              //对新写入的数据进行深度代理
+              value = deepProxy(value, (() => {
+                if (path !== undefined) {
+                  if (item instanceof Array) {
+                    return path + "[" + key + "]";
+                  } else return path + "." + key;
+                } else return key;
+              })(), "unset");
+            } else if (status === "setting") status = "unset";  //复位渲染状态
+            return Reflect.set(target, key, value, receiver);
+          },
+          deleteProperty(target, key) {  //当item属性被删除时的操作
+            status = "deleting";  //设定渲染状态为删除中
+            if (item instanceof Array) {  //当item为数组时
+              item = item.slice(0, item.length - 1);  //排除被删除的无效元素
+              if (path !== undefined) {
+                that.setData({ [path]: item });
+              } else that.setData(item);
+              status = "unset";  //复位渲染状态
+            } else {  //当item不是数组时
+              (function loop() {
+                setTimeout(() => {
+                  if (!(key in item)) {  //当被删除的属性已不在item中时再执行渲染
+                    if (path !== undefined) {
+                      that.setData({ [path]: item });
+                    } else that.setData({ [key]: null });
+                    status = "unset";  //复位渲染状态
+                  } else loop();  //当被删除的属性仍在item中时轮询等待
+                })
+              })()
             }
-          });
-        } else return item;
+            return Reflect.deleteProperty(target, key);
+          }
+        });
       } else return item;
     })(that["data"]);
   }
